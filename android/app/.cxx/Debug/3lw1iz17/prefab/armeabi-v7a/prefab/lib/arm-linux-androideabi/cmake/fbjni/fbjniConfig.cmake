8b852413c642d356d897193d95d54399db33eb62
balakrishnanair24 <balakrishnanair24@gmail.com>
2025-08-04 23:49:49 +0000

    API KEY SECURED GITIGNORED

---
+++ android/app/.cxx/Debug/3lw1iz17/prefab/armeabi-v7a/prefab/lib/arm-linux-androideabi/cmake/fbjni/fbjniConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET fbjni::fbjni)
+add_library(fbjni::fbjni SHARED IMPORTED)
+set_target_properties(fbjni::fbjni PROPERTIES
+    IMPORTED_LOCATION "/home/user/.gradle/caches/8.14.1/transforms/d72e83b2461edd281b6b7b42e2017bea/transformed/fbjni-0.7.0/prefab/modules/fbjni/libs/android.armeabi-v7a/libfbjni.so"
+    INTERFACE_INCLUDE_DIRECTORIES "/home/user/.gradle/caches/8.14.1/transforms/d72e83b2461edd281b6b7b42e2017bea/transformed/fbjni-0.7.0/prefab/modules/fbjni/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
